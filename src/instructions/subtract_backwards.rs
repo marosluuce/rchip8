0b125a9ea3cdf945d1d5f18a3c2de0c6eb51223e
Nathan Walker <earthworn@gmail.com>
2018-11-30 08:04:56 -0600

    Implement subtract backwards.

--- src/instructions/subtract_backwards.rs
+++ src/instructions/subtract_backwards.rs
@@ -2,18 +2,18 @@ use cpu::Cpu;
 use instructions::instruction::Instruction;
 use instructions::op::Op;
 
-struct And {
+struct SubtractBackwards {
     register1: usize,
     register2: usize,
 }
 
-impl Op for And {
-    const MASK: u16 = 0x8FF2;
+impl Op for SubtractBackwards {
+    const MASK: u16 = 0x8FF7;
 }
 
-impl Instruction for And {
-    fn new(opcode: u16) -> And {
-        And {
+impl Instruction for SubtractBackwards {
+    fn new(opcode: u16) -> SubtractBackwards {
+        SubtractBackwards {
             register1: ((opcode & 0x0F00) >> 8) as usize,
             register2: ((opcode & 0x00F0) >> 4) as usize,
         }
@@ -24,7 +24,13 @@ impl Instruction for And {
             pc: cpu.pc + 1,
             registers: {
                 let mut registers = cpu.registers;
-                registers[self.register1] &= registers[self.register2];
+                if registers[self.register1] < registers[self.register2] {
+                    registers[0xF] = 1;
+                } else {
+                    registers[0xF] = 0;
+                }
+                registers[self.register1] =
+                    registers[self.register2].wrapping_sub(registers[self.register1]);
                 registers
             },
             ..cpu
@@ -37,14 +43,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn loads_register2_into_register1() {
-        let instruction = And::new(0x8281);
+    fn subtracts_two_registers() {
+        let instruction = SubtractBackwards::new(0x8247);
         let cpu = Cpu {
             pc: 4,
             registers: {
                 let mut registers = [0; 16];
-                registers[2] = 0x04;
-                registers[8] = 0xFF;
+                registers[2] = 2;
+                registers[4] = 2;
+                registers[0xF] = 5;
                 registers
             },
             ..Cpu::new()
@@ -55,8 +62,39 @@ mod tests {
                 pc: 5,
                 registers: {
                     let mut registers = [0; 16];
-                    registers[2] = 0x04;
-                    registers[8] = 0xFF;
+                    registers[2] = 0;
+                    registers[4] = 2;
+                    registers[0xF] = 0;
+                    registers
+                },
+                ..cpu
+            },
+            instruction.execute(cpu)
+        );
+    }
+
+    #[test]
+    fn subtracts_two_registers_with_no_borrow() {
+        let instruction = SubtractBackwards::new(0x8245);
+        let cpu = Cpu {
+            pc: 4,
+            registers: {
+                let mut registers = [0; 16];
+                registers[2] = 3;
+                registers[4] = 5;
+                registers
+            },
+            ..Cpu::new()
+        };
+
+        assert_eq!(
+            Cpu {
+                pc: 5,
+                registers: {
+                    let mut registers = [0; 16];
+                    registers[2] = 2;
+                    registers[4] = 5;
+                    registers[0xF] = 1;
                     registers
                 },
                 ..cpu
